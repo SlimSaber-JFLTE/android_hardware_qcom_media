59188d4a8170941faa2a5f92889204fd14e1392b
B--B <dark.schneider.ds@gmail.com>
2016-02-15 19:14:29 +0100

    Fix path for SlimSaber

--- Android.mk
+++ Android.mk
@@ -1,9 +1,5 @@
-ifeq ($(call my-dir),$(call project-path-for,qcom-media))
-
-ifneq ($(filter msm8960,$(TARGET_BOARD_PLATFORM)),)
-
+ifeq ($(TARGET_QCOM_MEDIA_VARIANT),caf-msm8960)
+ifneq ($(filter msm8960 msm8660 msm7x27a msm7x30,$(TARGET_BOARD_PLATFORM)),)
 include $(call all-subdir-makefiles)
-
 endif
-
 endif
